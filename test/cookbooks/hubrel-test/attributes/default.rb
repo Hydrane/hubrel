a4b3db5f24afa38aab8923b3606ed862d7cff24f
Robert Vežnaver <rveznaver@users.noreply.github.com>
2017-06-06 23:31:08 +0200

    InSpec tests

--- test/cookbooks/hubrel-test/attributes/default.rb
+++ test/cookbooks/hubrel-test/attributes/default.rb
@@ -1,5 +1,6 @@
-release = Hubrel::Repo.new(owner: 'github', name: 'hub').releases.latest
-asset   = release.asset(name: 'hub-linux-amd64.+')
+asset = Hubrel::Repo.new(owner: 'github', name: 'hub')
+                    .releases.stable.latest
+                    .asset(name: 'hub-linux-amd64.+')
 
 default['pkg']['name'] = asset[:name]
 default['pkg']['url']  = asset[:url]
